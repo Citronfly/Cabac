d97d9fd169a8d2a9317d05740b3dabbd06be47a4
Citronfly <37669985+Citronfly@users.noreply.github.com>
2018-05-24 14:15:45 +0200

    bigger update then before but not big at all

--- client/client.js
+++ client/client.js
@@ -1,14 +1,15 @@
 $(function () {
 var socket = io();
-var bruger = "Bob";
-$('#bruger').val(bruger);
+var INITIAL_USERNAME = "Bob";
+var AVATAR_SIZE = 15;
+$('#user').val(user);
 $('form').submit(function(){
-  socket.emit('chat message',{message: $('#m').val(), bruger: $('#bruger').val()});
+  socket.emit('chat message',{message: $('#m').val(), user: $('#user').val()});
   $('#m').val('');
   return false;
 });
 socket.on('chat message', function(msg){
-  var avatar = '<img src="https://api.adorable.io/avatars/15/' + msg.bruger + '.png" style="width: 15px; height: 15px; border-radius: 50%;">';
- $('#messages').append($('<li>').html(avatar+ ' ' + msg.bruger + ' ' + 'says' + ':' + ' ' + msg.message));
+  var avatar = '<img src="https://api.adorable.io/avatars/15/' + msg.user + '.png" style="width: AVATAR_SIZEpx; height: AVATAR_SIZEpx; border-radius: 50%;">';
+ $('#messages').append($('<li>').html(avatar+ ' ' + msg.user + ' ' + 'says' + ':' + ' ' + msg.message));
 });
 });
